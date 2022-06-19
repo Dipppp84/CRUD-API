73f8da6dc588901d7c2854017b0b422084338d3e
dippp <Dipppp84@gmail.com>
2022-06-19 15:53:19 +0300

    fix: all feat: server.test, scripts "test"

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,35 @@ import * as path from 'path';
 import controller from './controller/Controller.js';
 import {handlerError} from "./service/ErrorService.js";
 import 'dotenv/config';
+import {cpus} from 'os';
+import cluster from 'cluster'
 
-const server = http.createServer(async (req, res) => {
+const port = process.env.PORT || 3000;
+const pid = process.pid;
+
+export const server = http.createServer(async (req, res) => {
     const url = path.parse(path.normalize(req.url.toLowerCase()));
+    res.setHeader('PID', pid)
     try {
         await controller(url, req, res);
-        console.log('asf')
     } catch (err) {
         handlerError(res, err);
     }
 });
-server.listen(process.env.PORT, () => {
-    console.log('Server is running on port ' + process.env.PORT);
-});
+if (process.argv[2] === 'multi') {
+    if (cluster.isPrimary) {
+        console.log(`Primary PID: ${pid}`);
+        const numCPUs = cpus().length;
+        console.log(`Starting ${numCPUs} forks`);
+        for (let i = 0; i < numCPUs; i++) {
+            cluster.fork();
+        }
+        console.log('Server is running on port ' + port);
+    } else {
+        server.listen(port);
+        console.log(`Worker PID: ${pid} started`);
+    }
+} else {
+    server.listen(port);
+    console.log('Server is running on port ' + port);
+}
